6b7cc290f6a20c82e64f85fd48d3113d61d431b7
uwulises <ulises.campodonico@ug.uchile.cl>
2023-06-11 18:55:45 -0400

    add input device handler library

--- src/joy/JoyRPI.py
+++ src/joy/JoyRPI.py
@@ -4,25 +4,35 @@ import sys
 sys.path.append('../')
 from serial_control.SerialControl import SerialControl
 import time 
-
-global base_comm
-global previous_x_value
-global previous_y_value
-previous_x_value = 0
-previous_y_value = 0
-
+  
 def process_joystick(x_val,y_val):
 
-    if  x_val != previous_x_value:
-        base_comm.forward() if x_val>0 else base_comm.backward()
-    elif y_val != previous_y_value:
-        base_comm.lateral_right() if y_val>0 else base_comm.lateral_left()
+    x_val=round(x_val)
+    y_val=round(y_val)
+
+    if (x_val==0 and y_val==0):
+        base_comm.stop()
+
+    elif (y_val==1 and x_val==0):
+        base_comm.forward()
+
+    elif (y_val==-1 and x_val==0):
+        base_comm.backward()
+
+    elif (x_val==1 and y_val==0):
+        base_comm.lateral_right()
+
+    elif (x_val==-1 and y_val==0):
+        base_comm.lateral_left()
     
-    previous_x_value = x_val
-    previous_y_value = y_val
+    elif (x_val==0 and y_val==0):
+        base_comm.stop()
 
 def main():
-    
+    global x_axis
+    global y_axis
+    x_axis=0
+    y_axis=0
     # Find Xbox wireless controller
     gamepad = None
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
@@ -37,8 +47,6 @@ def main():
     for event in gamepad.read_loop():
         if event.type == ecodes.EV_ABS:
             absevent = categorize(event)
-            x_axis=0
-            y_axis=0 
             if absevent.event.code == ecodes.ABS_X:
                 x_axis = absevent.event.value / 32767.0
             elif absevent.event.code == ecodes.ABS_Y:
@@ -46,7 +54,7 @@ def main():
             process_joystick(x_axis,y_axis)
 
 if __name__ == "__main__":
-    base_comm = SerialControl(port="/dev/ttyACM0")
+    base_comm = SerialControl()
     base_comm.open_serial()
     time.sleep(1)
     main()
