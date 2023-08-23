23183b58e9534f5044ceb0405a50d1db835a73c2
uwulises <ulises.campodonico@ug.uchile.cl>
2023-08-22 23:25:37 -0400

    add first try

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -75,12 +75,51 @@ void serialReading() {
     if (inputString == "STOP\n") {
       STOP();
     }
+    //take the 6 first characters of the string
+    //and compare it with "CMDVEL" 
+    if (inputString.substring(0, 6) == "CMDVEL") {
+      //take the 7th character of the string
+      int axial_speed = inputString.substring(6, 9).toInt();
+      int angular_speed = inputString.substring(9, 12).toInt();
+      // divide by 10.0 and convert to float to get the decimal point
+      float axial_speed_f = axial_speed / 10.0;
+      float angular_speed_f = angular_speed / 10.0;
+      //call cmd vel function
+      cmd_vel(axial_speed_f, angular_speed_f);
+    }
     // clear the string:
     inputString = "";
     stringComplete = false;
   }
 }
 
+//call motors function to move the robot, takes an array of 4 floats as argument
+void motors(float w[4]) {
+}
+
+// cmd_vel function that takes two floats as arguments of mecanum wheeled robot
+void cmd_vel(float vx, float vy) {
+  // Te distance between the center of the robot and the wheel D=0.1m and the radius of the wheel R=0.05m
+  float D = 0.15;
+  float R = 0.05;
+  int w_max = 400;
+  //create a v array to store the speed of each wheel
+  float w[4];
+  w[0] = (vx - vy)/R;
+  w[1] = (vx + vy)/R;
+  w[2] = (vx + vy)/R;
+  w[3] = (vx - vy)/R;
+  // transform v from m/s to rpm, to 0-255 pwm given that the maximun speed of the motor is 400rpm
+  for (int i = 0; i < 4; i++) {
+    w[i] = w[i] * 60 / (2 * PI * R);
+    w[i] = w[i] * 255 / w_max;
+  }
+
+   //call a function that takes the v array as argument and move the wheels.
+  motors(w);
+
+  
+}
 void FW() {
   motor1.run(FORWARD);
   motor2.run(FORWARD);
