7941cd260f75edb5189d9443fd931d4b084a5eb9
uwulises <ulises.campodonico@ug.uchile.cl>
2023-08-25 11:46:37 -0400

    add right sticj

--- src/joy/JoyRPI.py
+++ src/joy/JoyRPI.py
@@ -7,26 +7,36 @@ import time
 import numpy as np
 
 # Process joystick input
-def process_joystick(x_val=128,y_val=128):
-
+def process_joystick(x_val=128,y_val=128,rs_x_val=128,rs_y_val=128):
+    #round the values
     x_val=round(x_val)
     y_val=round(y_val)
+    rs_x_val=round(rs_x_val)
+    rs_y_val=round(rs_y_val)
     #create a vector with the joystick values
-    joystick_vector=(x_val,y_val)
-
+    joystick_vector_ls=(x_val,y_val)
+    joystick_vector_rs=(rs_x_val,rs_y_val)
     # Match the vector to command
-    switcher = {
+    switcher_ls = {
         (128,128): 'stop',
         (128,0): 'forward',
         (128,255): 'backward',
         (0,128): 'spin_left',
         (255,128): 'spin_right',
         }
-
-    func = switcher.get(joystick_vector)
-    if func is None:
-        func = 'stop'
-    base_comm.send_command(func)
+    # Match the vector to command
+    switcher_rs = {
+        (128,128): 'stop',
+        
+        }
+    func_ls = switcher_ls.get(joystick_vector_ls)
+    func_rs = switcher_rs.get(joystick_vector_rs)
+    if func_ls is None:
+        func_ls = 'stop'
+    elif func_rs is None:
+        func_rs = 'stop'    
+    base_comm.send_command(func_ls)
+    base_comm.send_command(func_rs)
     
 
 def main():
@@ -56,8 +66,12 @@ def main():
                 x_axis = absevent.event.value
             elif absevent.event.code == ecodes.ABS_Y:
                 y_axis = absevent.event.value
+            elif absevent.event.code == ecodes.ABS_Z:
+                rs_x_axis = absevent.event.value
+            elif absevent.event.code == ecodes.ABS_RZ:
+                rs_y_axis = absevent.event.value
             process_joystick(x_axis,y_axis)
-            print(x_axis,y_axis)
+            print(x_axis,y_axis,rs_x_axis,rs_y_axis)
 
 if __name__ == "__main__":
     base_comm = SerialControl()
