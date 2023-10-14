26a5656c0dd2f9edaac9a3d456bd727c26f0e5b7
uwulises <ulises.campodonico@ug.uchile.cl>
2023-10-14 20:09:40 -0300

    remove debug lines

--- src/examples/client_video_example.py
+++ src/examples/client_video_example.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from client.omnibase_robot_client import RobotClient
-import time
+import time   
 import cv2
 import numpy as np
 
