10241d73e27c23bd61ceae50492e2e140d39e60f
uwulises <ulises.campodonico@ug.uchile.cl>
2023-10-13 21:29:55 -0300

    add first try webrtc

---
+++ src/streamWebRTC/robot.py
@@ -0,0 +1,48 @@
+import requests
+from WebRTC import WebRTCController
+from videoShow import VideoShow
+
+
+class RobotClient:
+
+    def __init__(self, address, port=5000, portVideo=8080):
+        self.address = address
+        self.port = port
+        self.base_url = f"http://{address}:{port}"
+        self.connected = False
+        self.webRTCUser = WebRTCController(self.address)
+
+    def connect(self):
+        if self.connected:
+            print("already connected :)")
+            return
+        url = f"{self.base_url}/connect"
+        response = requests.get(url)
+        if response.status_code == 200:
+            self.connected = True
+            print(response.text)
+
+    def set_joints(self, q0=0, q1=0, q2=90, q3=120):
+        params = {"q0": q0, "q1": q1, "q2": q2}
+        url = f"{self.base_url}/set_joints"
+        response = requests.get(url, params=params)
+        print(response.text)
+
+    
+    def connectWebRTC(self):
+        self.webRTCUser.connect()
+
+    def closeWebRTC(self):
+        self.webRTCUser.close()
+        
+
+    def showVideo(self, process= lambda frame : (frame, None)):
+        self.webRTCUser.showVideo(process)
+    
+    def stopVideo(self):
+        self.webRTCUser.stopVideo()
+    
+
+    def get_frame(self):
+        return self.webRTCUser.getFrame()
+        
