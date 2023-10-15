c68abe93b8a81d923c5c79c9a1df7c37913b8b15
uwulises <ulises.campodonico@ug.uchile.cl>
2023-10-14 22:47:44 -0300

    update sh driver

--- start_driver.sh
+++ start_driver.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# Start a screen session named "driver"
-screen -S driver
-
-# Change directory to the project
-cd ~/ME5150_omnibase
-
-# Source the virtual environment
-source robotica/bin/activate
-
-# Change directory to the source folder
-cd src/serial_control
-
-# Run the Python script
-python3 drive_server.py
+screen -dmS driver bash -c 'cd ~/ME5150_omnibase && source robotica/bin/activate && cd src/serial_control && python3 drive_server.py'
