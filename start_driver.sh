51d132b49a3b7196bfd3f08fb095ca14fd0851ba
uwulises <ulises.campodonico@ug.uchile.cl>
2023-10-14 22:50:08 -0300

    update

--- start_driver.sh
+++ start_driver.sh
@@ -1,1 +1,1 @@
-screen -dmS driver bash -c 'cd ~/ME5150_omnibase && source robotica/bin/activate && cd src/serial_control && python3 drive_server.py'
+screen -X -S driver quit; screen -dmS driver bash -c 'cd ~/ME5150_omnibase && source robotica/bin/activate && cd src/serial_control && python3 drive_server.py'
