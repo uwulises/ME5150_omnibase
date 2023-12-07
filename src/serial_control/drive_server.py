34b105465b5c91300790935a097a0347da74128b
uwulises <ulises.campodonico@ug.uchile.cl>
2023-12-06 23:46:55 -0300

    add threaded

--- src/serial_control/drive_server.py
+++ src/serial_control/drive_server.py
@@ -22,4 +22,4 @@ def move(command):
     return "Command received: " + command
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,Threaded=True)
+    app.run(host='0.0.0.0', port=5000,threaded=True)
