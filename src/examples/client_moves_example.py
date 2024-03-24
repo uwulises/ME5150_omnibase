d33813075dd0543aa6423551d300ec6f6db55abb
leslieecv <leslie.cardenas008@gmail.com>
2024-03-24 14:33:02 -0300

    Update example commands

--- src/examples/client_moves_example.py
+++ src/examples/client_moves_example.py
@@ -4,9 +4,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from client.omnibase_robot_client import RobotClient
 import time   
 
-robot = RobotClient(address="omni.local")
-time.sleep(1)
+ip = '192.168.139.199' # "omni2.local"
+
+robot = RobotClient(address=ip)
+
+time.sleep(3)
+
+robot.send_move_command('stop')
+time.sleep(2)
+
 robot.send_move_command('forward')
+time.sleep(2)
+
 robot.send_move_command('stop')
-robot.send_move_command('backward')
-robot.send_move_command('stop')
+time.sleep(2)
