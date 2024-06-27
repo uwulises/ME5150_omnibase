9c5d03b416667e1ed5c5a3f321f4150ada399610
leslieecv <leslie.cardenas008@gmail.com>
2024-06-27 11:22:10 -0400

    change port

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ def main():
     dt = 0.2
     T_max = 2
     qf = [1, 0, 0]
-    sv = SendVelocities.SendVelocities(port = "COM6")
+    sv = SendVelocities.SendVelocities(port = "/dev/ttyACM0")
     jtraj_dt = GetTrajectory.GetTrajectory(qf, T_max, dt=dt)
     data = ""
     while "OK1" not in data:
